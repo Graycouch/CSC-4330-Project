658b4efad4219e5784846942c25b6a0e2d59c00e
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-10-17 21:06:18 -0500

    Added basic backend for user registration and log

---
+++ node-rest-api/Models/User.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            min: 3,
+            max: 20,
+            unique: true
+        },
+
+        email: {
+            type: String,
+            require: true,
+            max: 50,
+            unique: true
+        },
+
+        password: {
+            type: String,
+            require: true,
+            min: 6
+        },
+
+        profilePicture: {
+            type: String,
+            default: ""
+        },
+
+        courses: {
+            type: Array,
+            default: []
+        },
+
+        description: {
+            type: String,
+            max: 500,
+            default: ""
+        },
+
+        city: {
+            type: String,
+            max: 50,
+            default: ""
+        },
+
+        zipCode: {
+            type: String,
+            max: 50,
+            default: ""
+        },
+
+        hourlyRate: {
+            type: Number,
+            default: 0
+        },
+
+        isTutor: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
