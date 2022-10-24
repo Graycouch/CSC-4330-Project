a2761a538db5ad6facf3cfb3162921f85859cea1
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-10-24 12:33:19 -0500

    Upload changes in profile to mongodb

--- react-app-design/navigation/screens/ProfileScreen/ProfileScreen.js
+++ react-app-design/navigation/screens/ProfileScreen/ProfileScreen.js
@@ -4,10 +4,18 @@ import { View, Text, Button, Image, TextInput, Pressable, ScrollView, StyleSheet
 import { Divider } from '@rneui/themed';
 import { Dropdown } from 'react-native-element-dropdown';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import axios from 'axios';
+import localhost from 'react-native-localhost';
 
 export default function ProfileScreen({ navigation }) {
     const [user] = useGlobalState("user");
     const [editing, setEditing] = useState(false)
+    const [fullName, setFullName] = useState(user.fullName)
+    const [email, setEmail] = useState(user.email)
+    const [role, setRole] = useState(user.role)
+    const [major, setMajor] = useState(user.major)
+    const [courses, setCourses] = useState(user.courses)
+    const [about, setAbout] = useState(user.about)
 
     const handleLogOutClick = (e) => {
         e.preventDefault();
@@ -16,7 +24,27 @@ export default function ProfileScreen({ navigation }) {
 
     const handleEditProfileClick = (e) => {
         e.preventDefault();
-        setEditing(!editing);
+        setEditing(true);
+    }
+
+    const handleDoneClick = (e) => {
+        e.preventDefault();
+
+        try {
+            axios.put(`http://${localhost}:8800/api/users/${user._id}`, {
+                userId: user._id,
+                fullName: fullName,
+                email: email,
+                role: role,
+                major: major,
+                courses: courses,
+                about: about
+            });
+        } catch (err) {
+            console.log(err);
+        }
+
+        setEditing(false);
     }
 
     const styles = StyleSheet.create({
@@ -49,7 +77,7 @@ export default function ProfileScreen({ navigation }) {
                     <Pressable backgroundColor={'#FFFFFF'} style={{
                         right: -130, top: -40, height: 40, width: 100, borderRadius: 40,
                         borderColor: "#5F59F7", borderWidth: 1, alignItems: 'center', justifyContent: 'center'
-                    }} onPress={handleEditProfileClick}>
+                    }} onPress={handleDoneClick}>
                         <Text style={{ color: '#5F59F7', fontSize: 16 }}>
                             Done
                         </Text>
@@ -64,12 +92,12 @@ export default function ProfileScreen({ navigation }) {
                         <Text style={{ fontSize: 18, paddingBottom: 30 }}>
                             Full Name:
                         </Text>
-                        <TextInput style={{
+                        <TextInput onChangeText={text => setFullName(text)} style={{
                             fontSize: 15, width: 230, borderRadius: 20,
                             position: 'absolute', color: 'black', borderColor: '#9E9E9E', backgroundColor: '#F1F1F1', borderWidth: 1, marginLeft: 100, top: -5, paddingLeft: 10, paddingRight: 10
                         }}>
                             <Text style={{ paddingLeft: 20 }}>
-                                {user.fullName}
+                                {fullName}
                             </Text>
                         </TextInput>
 
@@ -77,12 +105,12 @@ export default function ProfileScreen({ navigation }) {
                         <Text style={{ fontSize: 18, paddingBottom: 30 }}>
                             Email:
                         </Text>
-                        <TextInput style={{
+                        <TextInput onChangeText={text => setEmail(text)} style={{
                             fontSize: 15, width: 230, borderRadius: 20,
                             position: 'absolute', color: 'black', borderColor: '#9E9E9E', backgroundColor: '#F1F1F1', borderWidth: 1, marginLeft: 100, top: 50, paddingLeft: 10, paddingRight: 10
                         }}>
                             <Text style={{ paddingLeft: 20 }}>
-                                {user.email}
+                                {email}
                             </Text>
                         </TextInput>
 
@@ -90,45 +118,45 @@ export default function ProfileScreen({ navigation }) {
                         <Text style={{ fontSize: 18, paddingBottom: 30 }}>
                             Role:
                         </Text>
-                        <TextInput style={{
+                        <TextInput onChangeText={text => setRole(text)} style={{
                             fontSize: 15, width: 230, borderRadius: 20,
                             position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 105, paddingLeft: 10, paddingRight: 10
                         }}>
-                            {user.role}
+                            {role}
                         </TextInput>
 
 
                         <Text style={{ fontSize: 18, paddingBottom: 30 }}>
                             Major:
                         </Text>
-                        <TextInput style={{
+                        <TextInput onChangeText={text => setMajor(text)} style={{
                             fontSize: 15, width: 230, borderRadius: 20,
                             position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 160, paddingLeft: 10, paddingRight: 10
                         }}>
-                            {user.major}
+                            {major}
                         </TextInput>
 
 
                         <Text style={{ fontSize: 18, paddingBottom: 30 }}>
                             Courses:
                         </Text>
-                        <TextInput style={{
+                        <TextInput onChangeText={text => setCourses(text)} style={{
                             fontSize: 15, width: 230, borderRadius: 20,
                             position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 215, paddingLeft: 10, paddingRight: 10
                         }}>
-                            {user.courses[0]}
+                            {courses}
                         </TextInput>
 
 
                         <Text style={{ fontSize: 18 }}>
                             About:
                         </Text>
-                        <TextInput multiline={true} style={{
+                        <TextInput multiline={true} onChangeText={text => setAbout(text)} style={{
                             fontSize: 15, width: 230, borderRadius: 20, paddingTop: 5,
                             position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 270, paddingLeft: 10, paddingRight: 10
                             , textAlignVertical: "top", height: 150
                         }}>
-                            {user.about}
+                            {about}
                         </TextInput>
                     </View>
 
@@ -157,7 +185,7 @@ export default function ProfileScreen({ navigation }) {
                         fontSize: 25, width: 300, borderRadius: 20, textAlign: 'center',
                         fontWeight: '500', position: 'absolute', top: 240
                     }}>
-                        {user.fullName}
+                        {fullName}
                     </Text>
 
                     <Divider style={{ top: 50, alignSelf: 'stretch' }} />
@@ -169,23 +197,23 @@ export default function ProfileScreen({ navigation }) {
                         Email Address:
                     </Text>
                     <Text style={{ fontSize: 15, top: -140, left: -110, color: 'grey' }}>
-                        {user.email}
+                        {email}
                     </Text>
 
 
                     <Text style={{ fontSize: 15, paddingBottom: 20, top: -192, left: 50, fontWeight: 'bold', color: '#5F59F7' }}>
                         User Role:
                     </Text>
-                    <Text style={{ fontSize: 15, top: -202, left: 70, color: 'grey' }}>
-                        {user.role}
+                    <Text style={{ fontSize: 15, top: -202, left: 42, color: 'grey' }}>
+                        {role}
                     </Text>
 
 
                     <Text style={{ fontSize: 15, paddingBottom: 20, top: -155, left: -110, fontWeight: 'bold', color: '#5F59F7' }}>
                         University Major:
                     </Text>
-                    <Text style={{ fontSize: 15, top: -165, left: -110, color: 'grey' }}>
-                        {user.major}
+                    <Text style={{ fontSize: 15, top: -165, left: -105, color: 'grey' }}>
+                        {major}
                     </Text>
 
 
@@ -193,14 +221,14 @@ export default function ProfileScreen({ navigation }) {
                         University Courses:
                     </Text>
                     <Text style={{ fontSize: 15, top: -227, left: 55, color: 'grey' }}>
-                        {user.courses}
+                        {courses}
                     </Text>
 
                     <Text style={{ fontSize: 20, paddingBottom: 20, top: -180, fontWeight: 'bold', color: '#5F59F7' }}>
                         About
                     </Text>
-                    <Text style={{ fontSize: 15, top: -190, textAlign: 'center', color: 'grey' }}>
-                        {user.about}
+                    <Text style={{ fontSize: 15, top: -190, textAlign: 'center', color: 'grey', paddingLeft: 10, paddingRight: 10 }}>
+                        {about}
                     </Text>
 
                     <Pressable backgroundColor={'#5F59F7'} style={{ top: -50, height: 60, width: 320, borderRadius: 40, alignItems: 'center', justifyContent: 'center' }} onPress={handleLogOutClick}>
