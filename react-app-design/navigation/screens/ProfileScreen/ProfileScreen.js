a96c5da404f853997a844e26c60a1b548ccbdebb
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-10-23 23:17:01 -0500

    Made some styling changes

--- react-app-design/navigation/screens/ProfileScreen/ProfileScreen.js
+++ react-app-design/navigation/screens/ProfileScreen/ProfileScreen.js
@@ -24,6 +24,12 @@ export default function ProfileScreen({ navigation }) {
             paddingVertical: 50,
             alignItems: 'center',
             flexGrow: 1
+        },
+        verticleLine: {
+            height: 200,
+            top: 50,
+            width: 1,
+            backgroundColor: '#E5E5E5',
         }
     });
 
@@ -34,101 +40,177 @@ export default function ProfileScreen({ navigation }) {
 
     return (
         <View style={{ backgroundColor: '#FFFFFF', flex: 1 }}>
-            <ScrollView contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
-                <Text style={{ fontSize: 30, fontWeight: 'bold', left: -100 }}>
-                    {user.username}
-                </Text>
-
-                <Pressable backgroundColor={'#FFFFFF'} style={{
-                    right: -130, top: -40, height: 40, width: 100, borderRadius: 40,
-                    borderColor: '#5F59F7', borderWidth: 1, alignItems: 'center', justifyContent: 'center'
-                }} onPress={handleEditProfileClick}>
-                    <Text style={{ color: '#5F59F7', fontSize: 16 }}>
-                        {editing ? "Done" : "Edit Profile"}
+            {editing ? (
+                <ScrollView contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
+                    <Text style={{ fontSize: 30, fontWeight: '500', left: -100 }}>
+                        {user.username}
                     </Text>
-                </Pressable>
 
-                <Divider style={{ top: -30, alignSelf: 'stretch', backgroundColor: '#E5E5E5', height: 1 }} />
-
-                <Image source={require('../../../assets/nash.jpg')} style={{ top: -20, height: 120, width: 120, borderRadius: 60 }} />
-
-                <TextInput editable={editing} style={{
-                    fontSize: 25, width: 300, borderRadius: 20, textAlign: 'center',
-                    fontWeight: 'bold', position: 'absolute', top: 240, color: 'black', borderColor: editing ? '#9E9E9E' : '#FFFFFF',
-                    backgroundColor: editing ? '#F1F1F1' : '#FFFFFF', borderWidth: 1
-                }}>
-                    {user.fullName}
-                </TextInput>
-
-                <View style={{ alignItems: 'flex-start', display: 'flex', marginRight: 250, marginTop: 50 }}>
-                    <Text style={{ fontSize: 18, paddingBottom: 30 }}>
-                        Email:
-                    </Text>
-                    <TextInput editable={editing} style={{
-                        fontSize: 15, width: 220, borderRadius: 20,
-                        position: 'absolute', color: 'black', borderColor: '#9E9E9E', backgroundColor: '#F1F1F1', borderWidth: 1, marginLeft: 100, top: -5, textAlign: 'center'
-                    }}>
-                        <Text style={{ paddingLeft: 20 }}>
-                            {user.email}
+                    <Pressable backgroundColor={'#FFFFFF'} style={{
+                        right: -130, top: -40, height: 40, width: 100, borderRadius: 40,
+                        borderColor: "#5F59F7", borderWidth: 1, alignItems: 'center', justifyContent: 'center'
+                    }} onPress={handleEditProfileClick}>
+                        <Text style={{ color: '#5F59F7', fontSize: 16 }}>
+                            Done
                         </Text>
-                    </TextInput>
+                    </Pressable>
+
+                    <Divider style={{ top: -30, alignSelf: 'stretch', backgroundColor: '#E5E5E5', height: 1 }} />
+
+                    <Image source={require('../../../assets/nash.jpg')} style={{ top: -20, height: 120, width: 120, borderRadius: 60 }} />
+
+                    <View style={{ alignItems: 'flex-start', display: 'flex', marginRight: 250 }}>
+
+                        <Text style={{ fontSize: 18, paddingBottom: 30 }}>
+                            Full Name:
+                        </Text>
+                        <TextInput style={{
+                            fontSize: 15, width: 230, borderRadius: 20,
+                            position: 'absolute', color: 'black', borderColor: '#9E9E9E', backgroundColor: '#F1F1F1', borderWidth: 1, marginLeft: 100, top: -5, paddingLeft: 10, paddingRight: 10
+                        }}>
+                            <Text style={{ paddingLeft: 20 }}>
+                                {user.fullName}
+                            </Text>
+                        </TextInput>
 
 
-                    <Text style={{ fontSize: 18, paddingBottom: 30 }}>
-                        Role:
+                        <Text style={{ fontSize: 18, paddingBottom: 30 }}>
+                            Email:
+                        </Text>
+                        <TextInput style={{
+                            fontSize: 15, width: 230, borderRadius: 20,
+                            position: 'absolute', color: 'black', borderColor: '#9E9E9E', backgroundColor: '#F1F1F1', borderWidth: 1, marginLeft: 100, top: 50, paddingLeft: 10, paddingRight: 10
+                        }}>
+                            <Text style={{ paddingLeft: 20 }}>
+                                {user.email}
+                            </Text>
+                        </TextInput>
+
+
+                        <Text style={{ fontSize: 18, paddingBottom: 30 }}>
+                            Role:
+                        </Text>
+                        <TextInput style={{
+                            fontSize: 15, width: 230, borderRadius: 20,
+                            position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 105, paddingLeft: 10, paddingRight: 10
+                        }}>
+                            {user.role}
+                        </TextInput>
+
+
+                        <Text style={{ fontSize: 18, paddingBottom: 30 }}>
+                            Major:
+                        </Text>
+                        <TextInput style={{
+                            fontSize: 15, width: 230, borderRadius: 20,
+                            position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 160, paddingLeft: 10, paddingRight: 10
+                        }}>
+                            {user.major}
+                        </TextInput>
+
+
+                        <Text style={{ fontSize: 18, paddingBottom: 30 }}>
+                            Courses:
+                        </Text>
+                        <TextInput style={{
+                            fontSize: 15, width: 230, borderRadius: 20,
+                            position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 215, paddingLeft: 10, paddingRight: 10
+                        }}>
+                            {user.courses[0]}
+                        </TextInput>
+
+
+                        <Text style={{ fontSize: 18 }}>
+                            About:
+                        </Text>
+                        <TextInput multiline={true} style={{
+                            fontSize: 15, width: 230, borderRadius: 20, paddingTop: 5,
+                            position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 270, paddingLeft: 10, paddingRight: 10
+                            , textAlignVertical: "top", height: 150
+                        }}>
+                            {user.about}
+                        </TextInput>
+                    </View>
+
+                </ScrollView>
+            ) : (
+                <ScrollView contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
+
+                    <Text style={{ fontSize: 30, fontWeight: '500', left: -100 }}>
+                        {user.username}
                     </Text>
-                    <TextInput editable={editing} style={{
-                        fontSize: 15, width: 220, borderRadius: 20,
-                        position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 50, textAlign: 'center'
+
+                    <Pressable backgroundColor={'#FFFFFF'} style={{
+                        right: -130, top: -40, height: 40, width: 100, borderRadius: 40, backgroundColor: '#5F59F7',
+                        alignItems: 'center', justifyContent: 'center'
+                    }} onPress={handleEditProfileClick}>
+                        <Text style={{ color: 'white', fontSize: 16 }}>
+                            Edit Profile
+                        </Text>
+                    </Pressable>
+
+                    <Divider style={{ top: -30, alignSelf: 'stretch', backgroundColor: '#E5E5E5', height: 1 }} />
+
+                    <Image source={require('../../../assets/nash.jpg')} style={{ top: -20, height: 120, width: 120, borderRadius: 60 }} />
+
+                    <Text style={{
+                        fontSize: 25, width: 300, borderRadius: 20, textAlign: 'center',
+                        fontWeight: '500', position: 'absolute', top: 240
                     }}>
+                        {user.fullName}
+                    </Text>
+
+                    <Divider style={{ top: 50, alignSelf: 'stretch' }} />
+                    <Divider style={{ top: 150, alignSelf: 'stretch' }} />
+                    <Divider style={{ top: 250, alignSelf: 'stretch' }} />
+                    <View style={styles.verticleLine}></View>
+
+                    <Text style={{ fontSize: 15, paddingBottom: 20, top: -130, left: -120, fontWeight: 'bold', color: '#5F59F7' }}>
+                        Email Address:
+                    </Text>
+                    <Text style={{ fontSize: 15, top: -140, left: -110, color: 'grey' }}>
+                        {user.email}
+                    </Text>
+
+
+                    <Text style={{ fontSize: 15, paddingBottom: 20, top: -192, left: 50, fontWeight: 'bold', color: '#5F59F7' }}>
+                        User Role:
+                    </Text>
+                    <Text style={{ fontSize: 15, top: -202, left: 70, color: 'grey' }}>
                         {user.role}
-                    </TextInput>
-
-
-
-                    <Text style={{ fontSize: 18, paddingBottom: 30 }}>
-                        Major:
                     </Text>
-                    <TextInput editable={editing} style={{
-                        fontSize: 15, width: 220, borderRadius: 20,
-                        position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 105, textAlign: 'center'
-                    }}>
+
+
+                    <Text style={{ fontSize: 15, paddingBottom: 20, top: -155, left: -110, fontWeight: 'bold', color: '#5F59F7' }}>
+                        University Major:
+                    </Text>
+                    <Text style={{ fontSize: 15, top: -165, left: -110, color: 'grey' }}>
                         {user.major}
-                    </TextInput>
-
-
-
-                    <Text style={{ fontSize: 18, paddingBottom: 30 }}>
-                        Courses:
                     </Text>
-                    <TextInput editable={editing} style={{
-                        fontSize: 15, width: 220, borderRadius: 20,
-                        position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, marginLeft: 100, top: 160, textAlign: 'center'
-                    }}>
-                        {user.courses[0]}
-                    </TextInput>
 
 
-
-                    <Text style={{ fontSize: 18 }}>
-                        About:
+                    <Text style={{ fontSize: 15, paddingBottom: 20, top: -217, left: 85, fontWeight: 'bold', color: '#5F59F7' }}>
+                        University Courses:
                     </Text>
-                    <TextInput multiline={true} editable={editing} style={{
-                        fontSize: 15, width: 320, borderRadius: 20, paddingTop: 5,
-                        position: 'absolute', color: 'black', backgroundColor: '#F1F1F1', borderColor: '#9E9E9E', borderWidth: 1, top: 250, textAlign: 'center', textAlignVertical: "top", height: 150
-                    }}>
+                    <Text style={{ fontSize: 15, top: -227, left: 55, color: 'grey' }}>
+                        {user.courses}
+                    </Text>
+
+                    <Text style={{ fontSize: 20, paddingBottom: 20, top: -180, fontWeight: 'bold', color: '#5F59F7' }}>
+                        About
+                    </Text>
+                    <Text style={{ fontSize: 15, top: -190, textAlign: 'center', color: 'grey' }}>
                         {user.about}
-                    </TextInput>
-                </View>
-
-
-
-                {/* <Pressable backgroundColor={'#5F59F7'} style={{ top: 90, height: 60, width: 320, borderRadius: 40, alignItems: 'center', justifyContent: 'center' }} onPress={handleLogOutClick}>
-                    <Text style={{ color: 'white', fontSize: 18 }}>
-                        Log Out
                     </Text>
-                </Pressable> */}
-            </ScrollView>
+
+                    <Pressable backgroundColor={'#5F59F7'} style={{ top: -50, height: 60, width: 320, borderRadius: 40, alignItems: 'center', justifyContent: 'center' }} onPress={handleLogOutClick}>
+                        <Text style={{ color: 'white', fontSize: 18 }}>
+                            Log Out
+                        </Text>
+                    </Pressable>
+
+                </ScrollView>
+            )}
         </View>
     )
 }
