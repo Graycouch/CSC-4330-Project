eba0e7d6053934a95b585a319c951c110e29bbc7
matthew-prutz <prutzm@gmail.com>
2022-11-01 13:51:10 -0500

    Added Helper Functions
    Added helper functions to see the contents of the Users and delete extra junk users.

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -10,6 +10,9 @@ const path = require("path");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 
+const User = require("./Models/User");
+
+
 dotenv.config();
 
 // Connection to MongoDB
@@ -17,8 +20,42 @@ mongoose.connect(process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
         console.log("Successfully Connected to MongoDB!");
+
+
+        //
+        //  Debug helper functions can be called here:
+        //
+        displayUsers();
+        //deleteUser();
     }
 );
+
+// 
+//  For Debug Purposes  (Displays all the users in databse)
+//
+async function displayUsers() {
+    try {
+      const users = await User.find({});
+      console.log(users);
+                }
+    catch(err) {
+      handleError(err);
+    }
+}
+
+// 
+//  For Debug Purposes  (Deletes a User)
+//
+async function deleteUser() {
+    try {
+      const users = await User.deleteOne({ username: 'Matthew3' });;
+      console.log(users);
+                }
+    catch(err) {
+      handleError(err);
+    }
+}
+
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
