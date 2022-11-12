1f258f4007c7e86dbe697c713127e96cd991da7a
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-11-11 23:37:34 -0600

    Added some tiny touches to the message screen

--- react-app-design/navigation/screens/MessageScreen/MessageScreen.js
+++ react-app-design/navigation/screens/MessageScreen/MessageScreen.js
@@ -136,74 +136,122 @@ export default function MessageScreen({ navigation }) {
     return (
         <View style={{ backgroundColor: '#FFFFFF', flex: 1 }}>
             {replying ? (
-                <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
-                    <View style={{ position: 'absolute', flexDirection: 'row', top: windowWidth * 0.15 }}>
+                messages.length !== 0 ? (
+                    <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
+                        <View style={{ position: 'absolute', flexDirection: 'row', top: windowWidth * 0.15 }}>
 
-                        <View style={{
-                            height: windowHeight * 0.06144, width: windowHeight * 0.06144,
-                            borderRadius: windowHeight * 0.03072, top: -windowHeight * 0.01, marginRight: windowWidth * 0.02
-                        }}>
-                            <Image source={{ uri: chatUser.profilePicture === "" ? publicFolder + "defaultProfilePicture.png" : chatUser.profilePicture }}
-                                style={{
-                                    height: windowHeight * 0.06144, width: windowHeight * 0.06144,
-                                    borderRadius: windowHeight * 0.03072
-                                }} />
+                            <View style={{
+                                height: windowHeight * 0.06144, width: windowHeight * 0.06144,
+                                borderRadius: windowHeight * 0.03072, top: -windowHeight * 0.01, marginRight: windowWidth * 0.02
+                            }}>
+                                <Image source={{ uri: chatUser.profilePicture === "" ? publicFolder + "defaultProfilePicture.png" : chatUser.profilePicture }}
+                                    style={{
+                                        height: windowHeight * 0.06144, width: windowHeight * 0.06144,
+                                        borderRadius: windowHeight * 0.03072
+                                    }} />
+                            </View>
+
+                            <Text style={{ fontSize: 25, fontWeight: '500' }}>
+                                {chatUser.username}
+                            </Text>
                         </View>
 
-                        <Text style={{ fontSize: 25, fontWeight: '500' }}>
-                            {chatUser.username}
-                        </Text>
-                    </View>
+                        <View style={styles.replyPageHorizontalLine1} />
 
-                    <View style={styles.replyPageHorizontalLine1} />
-
-                    <Pressable onPress={handleBackButtonClick}>
-                        <MaterialCommunityIcons name={"chevron-left"} color={"#5F59F7"} size={40} style={{ marginLeft: 'auto', marginRight: 'auto', left: -windowWidth * 0.42, top: windowHeight * 0.01 }} />
-                    </Pressable>
-
-                    <Pressable onPress={handlePhoneButtonClick}>
-                        <MaterialCommunityIcons name={"phone"} color={"#5F59F7"} size={30} style={{ marginLeft: 'auto', marginRight: 'auto', top: -windowHeight * 0.04, left: windowWidth * 0.4 }} />
-                    </Pressable>
-
-                    <View style={{ top: windowHeight * 0.68 }}>
-                        <TextInput placeholder="Send Message" style={{
-                            backgroundColor: '#F1F1F1', height: windowHeight * 0.0512, width: windowWidth * 0.8,
-                            borderRadius: windowHeight * 0.0256, paddingLeft: windowWidth * 0.05, fontSize: 15, left: -windowWidth * 0.07
-                        }} value={currentMessage} onChangeText={newText => setCurrentMessage(newText)} onSubmitEditing={handleSendButtonClick} />
-                        <Pressable onPress={handleSendButtonClick} backgroundColor={'#5F59F7'} style={{
-                            top: -windowHeight * 0.05, left: windowWidth * 0.76, height: windowHeight * 0.0512, width: windowHeight * 0.0512,
-                            borderRadius: windowHeight * 0.0512
-                        }}>
-                            <MaterialCommunityIcons name={"chevron-right"} color={"white"} size={40} />
+                        <Pressable onPress={handleBackButtonClick}>
+                            <MaterialCommunityIcons name={"chevron-left"} color={"#5F59F7"} size={40} style={{ marginLeft: 'auto', marginRight: 'auto', left: -windowWidth * 0.42, top: windowHeight * 0.01 }} />
                         </Pressable>
-                    </View>
 
-                    <View style={{ flexDirection: 'column' }}>
-                        {messages.map((message, index) => (
-                            message.sender === user._id ? (
-                                <View key={index} style={{
-                                    top: -windowHeight * 0.1, right: -windowWidth * 0.14, alignItems: 'flex-end',
-                                    paddingBottom: windowHeight * 0.01, width: windowWidth * 0.7
-                                }}>
-                                    <Text style={{ backgroundColor: '#5F59F7', borderRadius: windowHeight * 0.0256, paddingLeft: 15, paddingRight: 15, paddingVertical: 5, fontSize: 18, color: 'white' }}>
-                                        {message.text}
-                                    </Text>
-                                </View>
-                            ) : (
-                                <View key={index} style={{
-                                    top: -windowHeight * 0.1, left: -windowWidth * 0.14, alignItems: 'flex-start',
-                                    paddingBottom: windowHeight * 0.01, width: windowWidth * 0.7
-                                }}>
-                                    <Text style={{ backgroundColor: 'gray', borderRadius: windowHeight * 0.0256, paddingLeft: 15, paddingRight: 15, paddingVertical: 5, fontSize: 18, color: 'white' }}>
-                                        {message.text}
-                                    </Text>
-                                </View>
-                            )
-                        ))
-                        }
-                    </View>
-                </ScrollView>
-            ) : (
+                        <Pressable onPress={handlePhoneButtonClick}>
+                            <MaterialCommunityIcons name={"phone"} color={"#5F59F7"} size={30} style={{ marginLeft: 'auto', marginRight: 'auto', top: -windowHeight * 0.04, left: windowWidth * 0.4 }} />
+                        </Pressable>
+
+                        <View style={{ top: windowHeight * 0.68 }}>
+                            <TextInput placeholder="Send Message" style={{
+                                backgroundColor: '#F1F1F1', height: windowHeight * 0.0512, width: windowWidth * 0.8,
+                                borderRadius: windowHeight * 0.0256, paddingLeft: windowWidth * 0.05, fontSize: 15, left: -windowWidth * 0.07
+                            }} value={currentMessage} onChangeText={newText => setCurrentMessage(newText)} onSubmitEditing={handleSendButtonClick} />
+                            <Pressable onPress={handleSendButtonClick} backgroundColor={'#5F59F7'} style={{
+                                top: -windowHeight * 0.05, left: windowWidth * 0.76, height: windowHeight * 0.0512, width: windowHeight * 0.0512,
+                                borderRadius: windowHeight * 0.0512, alignItems: 'center', justifyContent: 'center'
+                            }}>
+                                <MaterialCommunityIcons name={"send"} color={"white"} size={25} />
+                            </Pressable>
+                        </View>
+
+                        <View style={{ flexDirection: 'column' }}>
+                            {messages.map((message, index) => (
+                                message.sender === user._id ? (
+                                    <View key={index} style={{
+                                        top: -windowHeight * 0.1, right: -windowWidth * 0.14, alignItems: 'flex-end',
+                                        paddingBottom: windowHeight * 0.01, width: windowWidth * 0.7
+                                    }}>
+                                        <Text style={{ backgroundColor: '#5F59F7', borderRadius: windowHeight * 0.0256, paddingLeft: 15, paddingRight: 15, paddingVertical: 5, fontSize: 18, color: 'white' }}>
+                                            {message.text}
+                                        </Text>
+                                    </View>
+                                ) : (
+                                    <View key={index} style={{
+                                        top: -windowHeight * 0.1, left: -windowWidth * 0.14, alignItems: 'flex-start',
+                                        paddingBottom: windowHeight * 0.01, width: windowWidth * 0.7
+                                    }}>
+                                        <Text style={{ backgroundColor: 'gray', borderRadius: windowHeight * 0.0256, paddingLeft: 15, paddingRight: 15, paddingVertical: 5, fontSize: 18, color: 'white' }}>
+                                            {message.text}
+                                        </Text>
+                                    </View>
+                                )
+                            ))
+                            }
+                        </View>
+                    </ScrollView>) : (
+                    <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
+                        <View style={{ position: 'absolute', flexDirection: 'row', top: windowWidth * 0.15 }}>
+
+                            <View style={{
+                                height: windowHeight * 0.06144, width: windowHeight * 0.06144,
+                                borderRadius: windowHeight * 0.03072, top: -windowHeight * 0.01, marginRight: windowWidth * 0.02
+                            }}>
+                                <Image source={{ uri: chatUser.profilePicture === "" ? publicFolder + "defaultProfilePicture.png" : chatUser.profilePicture }}
+                                    style={{
+                                        height: windowHeight * 0.06144, width: windowHeight * 0.06144,
+                                        borderRadius: windowHeight * 0.03072
+                                    }} />
+                            </View>
+
+                            <Text style={{ fontSize: 25, fontWeight: '500' }}>
+                                {chatUser.username}
+                            </Text>
+                        </View>
+
+                        <View style={styles.replyPageHorizontalLine1} />
+
+                        <Pressable onPress={handleBackButtonClick}>
+                            <MaterialCommunityIcons name={"chevron-left"} color={"#5F59F7"} size={40} style={{ marginLeft: 'auto', marginRight: 'auto', left: -windowWidth * 0.42, top: windowHeight * 0.01 }} />
+                        </Pressable>
+
+                        <Pressable onPress={handlePhoneButtonClick}>
+                            <MaterialCommunityIcons name={"phone"} color={"#5F59F7"} size={30} style={{ marginLeft: 'auto', marginRight: 'auto', top: -windowHeight * 0.04, left: windowWidth * 0.4 }} />
+                        </Pressable>
+
+                        <View style={{ top: windowHeight * 0.68 }}>
+                            <TextInput placeholder="Send Message" style={{
+                                backgroundColor: '#F1F1F1', height: windowHeight * 0.0512, width: windowWidth * 0.8,
+                                borderRadius: windowHeight * 0.0256, paddingLeft: windowWidth * 0.05, fontSize: 15, left: -windowWidth * 0.07
+                            }} value={currentMessage} onChangeText={newText => setCurrentMessage(newText)} onSubmitEditing={handleSendButtonClick} />
+                            <Pressable onPress={handleSendButtonClick} backgroundColor={'#5F59F7'} style={{
+                                top: -windowHeight * 0.05, left: windowWidth * 0.76, height: windowHeight * 0.0512, width: windowHeight * 0.0512,
+                                borderRadius: windowHeight * 0.0512, alignItems: 'center', justifyContent: 'center'
+                            }}>
+                                <MaterialCommunityIcons name={"send"} color={"white"} size={25} />
+                            </Pressable>
+                        </View>
+
+                        <Text style={{ fontSize: 15, fontWeight: '400', color: 'gray', top: -15, textAlign: 'center', top: windowHeight * 0.2, paddingRight: 15, paddingLeft: 15 }}>
+                            Start up a conversation with {chatUser.username}
+                        </Text>
+                    </ScrollView>
+                )
+            ) : conversationUsers.length !== 0 ? (
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFFFFF' }}>
                     <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
 
@@ -237,13 +285,12 @@ export default function MessageScreen({ navigation }) {
                                                         borderRadius: windowHeight * 0.0512
                                                     }} />
                                             </View>
-                                            <View style={{ alignItems: 'flex-start', top: -windowHeight * 0.095, paddingLeft: windowWidth * 0.286, paddingRight: windowWidth * 0.026 }}>
+                                            <View style={{ alignItems: 'flex-start', top: -windowHeight * 0.09, paddingLeft: windowWidth * 0.286, paddingRight: windowWidth * 0.026 }}>
                                                 <Text style={{ fontSize: 20, fontWeight: 'bold', paddingBottom: windowHeight * 0.0001 }}>
                                                     {currentUser.username}
                                                 </Text>
-                                                <Text style={{ color: 'gray', fontSize: 15 }}>
-                                                    Sure! I'd be happy to help you with that.
-                                                    What is a convenient time for you?
+                                                <Text style={{ color: 'gray', fontSize: 12, paddingTop: 5 }}>
+                                                    Click to send a message to {currentUser.username} and learn more about {currentUser.major}
                                                 </Text>
                                             </View>
 
@@ -252,6 +299,29 @@ export default function MessageScreen({ navigation }) {
                                 ))}
                             </>
                         </View>
+                    </ScrollView>
+                </View>
+            ) : (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFFFFF' }}>
+                    <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentContainer} style={{ backgroundColor: '#FFFFFF' }}>
+
+                        <View style={{ flexDirection: 'row', marginBottom: windowHeight * 0.0128, top: -windowHeight * 0.019 }}>
+                            <TextInput placeholder="Search" value={searchValue} onChangeText={newText => setGlobalState("searchValue", newText)}
+                                onSubmitEditing={() => navigation.navigate('Search', { screen: 'Search' })}
+                                style={{ backgroundColor: '#F1F1F1', height: windowHeight * 0.0512, width: windowWidth * 0.95, borderRadius: windowHeight * 0.0256, paddingLeft: windowWidth * 0.104, fontSize: 15 }} />
+                            <MaterialCommunityIcons name={"magnify"} color={"#9E9E9E"} size={20} style={{ top: windowHeight * 0.0128, position: 'absolute', paddingLeft: windowWidth * 0.026 }} />
+                        </View>
+
+                        <Text style={{ fontSize: 30, fontWeight: 'bold', top: -15 }}>
+                            Messages
+                        </Text>
+
+                        <Text style={{ fontSize: 20, fontWeight: '400', color: 'gray', top: -15, textAlign: 'center', top: windowHeight * 0.2, paddingRight: 5, paddingLeft: 5 }}>
+                            Help is available, try messaging {user.role.toLowerCase() === "student" ? "tutors" : "students"} on the search screen...
+                        </Text>
+                        <Text style={{ fontSize: 30, fontWeight: '400', color: 'gray', top: -15, textAlign: 'center', top: windowHeight * 0.25, paddingRight: 20, paddingLeft: 20 }}>
+                            (╯°□°)╯︵ ┻━┻
+                        </Text>
                     </ScrollView>
                 </View>
             )}
